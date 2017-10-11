7d168b5960e9e6b226bcc50fdcf39fdff75a774d
Hack Reactor Student <student@hackreactor.com>
2017-10-11 15:40:06 -0700

    Finish prototypal for stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  someInstance.head = -1;
+  return someInstance;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+  ++this.head;
+  this.storage[this.head] = value;
+};
+
+stackMethods.pop = function() {
+  var tmp = this.storage[this.head];
+  
+  delete this.storage[this.head--];
+  return tmp;
+};
+
+stackMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
 
 
