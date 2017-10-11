24ad5d1e44c99172b81d2129203cc67d3d8b02d4
Hack Reactor Student <student@hackreactor.com>
2017-10-11 14:50:24 -0700

    Finish functional implementation of stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,27 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var head = -1;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    ++head;
+    storage[head] = value;
   };
 
   someInstance.pop = function() {
+    if (someInstance.size === 0) {
+      head = -1;
+      return undefined;
+    }
+    var temp = storage[head];
+    
+    delete storage[head--]; // updates head
+    return temp;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
