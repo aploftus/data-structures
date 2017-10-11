f628ba035329db55b7c98b2cbef00b051baa55f1
Hack Reactor Student <student@hackreactor.com>
2017-10-11 15:53:04 -0700

    Finish pseudoclassical for queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.head = -1;
+  this.tail = -1;
 };
 
+Queue.prototype.enqueue = function(value) {
+  if (this.size() === 0) {
+    ++this.head;
+  }
+  ++this.tail;
+  this.storage[this.tail] = value;
+};
 
+Queue.prototype.dequeue = function() {
+  var tmp = this.storage[this.head];
+  
+  delete this.storage[this.head++];
+  if (this.size() === 0) {
+    this.head = -1;
+    this.tail = -1;
+  }
+  return tmp;
+};
+
+Queue.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
