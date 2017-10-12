5fe49399ef25a3a81b9b515faeb33029d1bddd21
Hack Reactor Student <student@hackreactor.com>
2017-10-12 15:47:30 -0700

    Instantiate bst with new keyword; add test for direction of traversal

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -2,7 +2,7 @@ describe('binarySearchTree', function() {
   var binarySearchTree;
 
   beforeEach(function() {
-    binarySearchTree = BinarySearchTree(5);
+    binarySearchTree = new BinarySearchTree(5);
   });
 
   it('should have methods named "insert", "contains", and "depthFirstLog', function() {
@@ -36,5 +36,16 @@ describe('binarySearchTree', function() {
     binarySearchTree.insert(7);
     binarySearchTree.depthFirstLog(func);
     expect(array).to.eql([5, 2, 3, 7]);
+  });
+  
+  it('should execute "depthFirstLog" on left branch first', function() {
+    var array = [];
+    var func = function(value) { array.push(value); };
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(7);
+    binarySearchTree.depthFirstLog(func);
+    expect(array).to.eql([5, 2, 3, 7]);
+    expect(array).to.not.eql([5, 7, 2, 3]);
   });
 });
