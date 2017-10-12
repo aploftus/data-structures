425e1862783ea131300110a4d391cee9ad95adee
Hack Reactor Student <student@hackreactor.com>
2017-10-12 15:47:49 -0700

    Finish bst

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,52 @@
 var BinarySearchTree = function(value) {
+  this.left = null;
+  this.right = null;
+  this.value = value;
+};
+
+BinarySearchTree.prototype.insert = function(value) { // O(log(n));
+  if (value < this.value) {
+    if (this.left === null) {
+      this.left = new BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if (this.right === null) {
+      this.right = new BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+BinarySearchTree.prototype.contains = function(value) { // O(log(n));
+  if (value === this.value) {
+    return true;
+  } else if (value < this.value) {
+    if (this.left === null) {
+      return false;
+    } else {
+      return this.left.contains(value);    
+    }
+  } else if (value > this.value) {
+    if (this.right === null) {
+      return false;
+    } else {
+      return this.right.contains(value);    
+    } 
+  }
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(callback) { // O(n);
+  callback(this.value);
+  
+  if (this.left !== null) {
+    this.left.depthFirstLog(callback);
+  }
+  if (this.right !== null) {
+    this.right.depthFirstLog(callback);
+  }
 };
 
 
