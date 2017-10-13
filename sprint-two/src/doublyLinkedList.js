47ce33aa9435211cdb611bab05077be19961d7a7
Hack Reactor Student <student@hackreactor.com>
2017-10-13 15:21:28 -0700

    Add DoublyLinkedList with addToTail and addToHead functionality

---
+++ sprint-two/src/doublyLinkedList.js
@@ -0,0 +1,90 @@
+var DoublyLinkedList = function() {
+  this.head = null;
+  this.tail = null;
+};
+
+DoublyLinkedList.prototype.addToTail = function(value) { // O = 1
+  var temp = new DoublyLinkedNode(value);
+  // if list is empty, then head and tail updated to first and only node
+  if (this.head === null && this.tail === null) {
+    this.head = temp;
+    this.tail = temp;
+  } else {
+    temp.prev = this.tail;
+    this.tail.next = temp;
+    this.tail = temp;
+  }
+};
+
+DoublyLinkedList.prototype.addToHead = function(value) {
+  var temp = new DoublyLinkedNode(value);
+  // if list is empty, then head and tail updated to first and only node
+  if (this.head === null && this.tail === null) {
+    this.head = temp;
+    this.tail = temp;
+  } else {
+    temp.next = this.head;
+    this.head.prev = temp;
+    this.head = temp;
+  }
+};
+
+DoublyLinkedList.prototype.removeHead = function() { // O = 1
+  if (this.head === null && this.tail === null) {
+    return undefined;
+  }
+  var temp = this.head.value;
+  this.head = this.head.next;
+  this.head.prev = null;
+  return temp;
+};
+
+DoublyLinkedList.prototype.removeTail = function() {
+  if (this.head === null && this.tail === null) {
+    return undefined;
+  }
+  var temp = this.head.value;
+  this.head = this.head.next;
+  return temp;
+};
+
+DoublyLinkedList.prototype.contains = function(target) { // O = n
+  var temp = this.head;
+  while (temp.value !== target) {
+    temp = temp.next;
+    if (temp == null) {
+      return false;
+    }
+  }
+  return true;
+};
+
+DoublyLinkedList.prototype.remove = function(target) {
+  var curr = this.head;
+  var subs = this.head.next;
+  
+  if (_.isEqual(curr.value, target)) {
+    this.removeHead();
+    return;
+  }
+  
+  while (subs !== null) {
+    if (_.isEqual(subs.value, target)) {
+      curr.next = subs.next;
+      return;
+    } else {
+      curr = subs;
+      subs = subs.next;
+    }
+  }
+};
+
+var DoublyLinkedNode = function(value) {
+  this.value = value;
+  this.next = null;
+  this.prev = null;
+};
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ */
