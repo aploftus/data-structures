9a36335b03d346756c7ad9d1ef5f912c035da6da
Hack Reactor Student <student@hackreactor.com>
2017-10-13 19:26:03 -0700

    Add dynamic resizing functionality for hashtable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,5 +1,3 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
@@ -8,16 +6,16 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) { // O(1)
   var index = getIndexBelowMaxForKey(k, this._limit);
-  // checking if a bucket has been created at index
-  if (this._storage.get(index) === undefined) {
+  // create an empty bucket if it hasn't been created yet
+  var bucket = this._storage.get(index);
+  if (bucket === undefined) {
     this._storage.set(index, LinkedList());
   }
-  // checking if k/v already exists
-  var bucket = this._storage.get(index);
+  bucket = this._storage.get(index);
   var current = bucket.head;
   
-  // reassign new value
-  while (current !== null) {
+  // reassign new value to existing key
+  while (current) {
     if (current.value[0] === k) {
       current.value[1] = v;
       return;
@@ -28,19 +26,22 @@ HashTable.prototype.insert = function(k, v) { // O(1)
   // if doesn't already exist, stores k/v tuple in bucket
   this._storage.get(index).addToTail([k, v]);
   ++this._counter;
-  this._rehash();
+  this.grow();
 };
 
 HashTable.prototype.retrieve = function(k) { //O(1)
   var index = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(index);
-  var current = bucket.head;
   
-  while (current !== null) {
-    if (current.value[0] === k) {
-      return current.value[1];
-    } else {
-      current = current.next;
+  if (bucket) {
+    var current = bucket.head;
+    
+    while (current) {
+      if (current.value[0] === k) {
+        return current.value[1];
+      } else {
+        current = current.next;
+      }
     }
   }
 };
@@ -51,47 +52,49 @@ HashTable.prototype.remove = function(k) { // O(1)
   var current = bucket.head;
   var target = [k, this.retrieve(k)];
   
-  while (current !== null) {
+  while (current) {
     if (current.value[0] === k) {
       bucket.remove(target);
+      --this._counter;
+      this.shrink();
       return;
     } else {
       current = current.next;
     }
-  } 
+  }
 };
 
-HashTable.prototype.resize = function() {
-  var oldStorage = this._storage;
-  // for EACH index in old LimitedArray, call rehash
-  var rehash = function(bucket) {
-    var current = bucket.head;
-    
-    while (current !== null) {
-      //insert(current.value[0])
-    }
-    // for each node in bucket
-    // grab the key (node[0]), and rehash to new LimitedArray (node[0], node[1]);
-    // use new hash to insert k/v pair in new LimitedArray
-  };
+HashTable.prototype.grow = function() {
+  if (this._counter / this._limit > 0.75) {
+    var oldStorage = this._storage;
   
-  // when counter / limit > .75
-    // double the limit
-    // rehash every item
-  if (this._counter / this._limit >= 0.75) {
     this._limit *= 2;
     this._storage = LimitedArray(this._limit);
-    oldStorage.each(rehash);
+    this._counter = 0;
+    oldStorage.each(this.rehash.bind(this));
   }
-   
-    
-  // when counter / limit < .25
-    // halve the limit
-    // rehash every item  
-  if (this._counter / this._limit <= 0.25) {
+};
+
+HashTable.prototype.shrink = function() {
+  if (this._counter / this._limit < 0.25) {
+    var oldStorage = this._storage;
+ 
     this._limit /= 2;
     this._storage = LimitedArray(this._limit);
-    oldStorage.each(rehash);
+    this._counter = 0;
+    oldStorage.each(this.rehash.bind(this));
+  }
+};
+
+HashTable.prototype.rehash = function(bucket) {
+  if (bucket) {
+    var current = bucket.head;
+    
+    while (current) {
+      this.insert(current.value[0], current.value[1]);
+      current = current.next;
+    }
+    
   }
 };
 
