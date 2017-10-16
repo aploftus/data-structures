09f5b4411981761b95e021212613f5443301a61d
aploftus <aploftus@users.noreply.github.com>
2017-10-15 21:40:35 -0700

    Add summary of Linked List behavior

---
+++ sprint-two/loftusSummary.js
@@ -0,0 +1,25 @@
+// Sprint-Two Summary
+
+// A linked list has a head and a tail.
+// It contains nodes that each have two characteristics: a value, and a Next pointer to the next node.
+// The tail's Next points to null;
+// We initialize the head and tail to point to null as well.
+// To insert at tail, we create a new Node, with a default Next pointing to null.
+//   If list is empty, then head and tail both point at the new Node.
+//   If list has contents, then the current tail's Next should point to the new Node,
+//     and the new Node's Next should point to null (it becomes the new tail).
+// To remove from head, we make the head's Next point to the Node that comes after the old head.
+//   (The head's Next will point to what the old Head's Next used to point to.)
+//   The old head will be garbage collected once nothing is pointing to it.
+// To check if the list contains an item, we can Search the list.
+//   If we find it, return true. If not, return false;
+// We can search the list for a target by tracking the item we are currently inspecting,
+//   and the item that comes after it.
+//   if the inspected item is the target, perform a "I found it!" action
+//   else, inspect the next item.
+//   if we hit the end of the list (if the next item is null), perform a "I didn't find it" action.
+// To remove an item from the middle of the list, we can search the list for the item
+//   In this case, we don't want to traverse until we are currently inspecting the item;
+//     rather, we traverse until we see that the item subsequent to our current position is the target
+//     then, we make the current's Next point to the target's Next.
+//     With nothing pointing to the target, it is garbage collected.
